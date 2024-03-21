0d358e54c2d4a16a4c38bf092bdcdb6cf0be1518
techspott580 <160092133+techspott580@users.noreply.github.com>
2024-03-21 15:47:34 +0530

    Add files via upload

---
+++ FEEDBACK/feedbackproject/testapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+from testapp.forms import FeedBackForm
+def feedback_view(request):
+    submitted=False
+    name=''
+    if request.method=='POST':
+        form=FeedBackForm(request.POST)
+        if form.is_valid():
+            print("Form validation success and printing feedback information")
+            print("*"*55)
+            print("Name:",form.cleaned_data['name'])
+            print("Rollno:",form.cleaned_data['rollno'])
+            print("MailID:",form.cleaned_data['email'])
+            print("Feedback:",form.cleaned_data['feedback'])
+            submitted=True
+            name=form.cleaned_data['name']
+    form=FeedBackForm()
+    return render(request,'testapp/feedback.html',{'form':form,'submitted':submitted,
+	'name':name})
